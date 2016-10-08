2750f9cce5669adb90189b19e0f8e701bfb8b4d8
achernyshev <AChernyshev@sphereconsultinginc.com>
2016-10-08 18:13:10 +0300

    switch to net instead of curl

---
+++ README.md
@@ -0,0 +1,26 @@
+### Requirements
+
+ruby 2.1.2 or newer
+
+### Usage
+
+* Login to [FUT web app](https://www.easports.com/ru/fifa/ultimate-team/web-app) in Chrome. 
+* Open chrome dev tools, Network tab
+* Make any request to transfer market, find it in dev tools and find headers that your browser send
+* Insert value of `X-UT-SID` header into `buy.rb` file as a value of `$session_id` variable
+* Insert value of `X-UT-PHISHING-TOKEN` header into `buy.rb` file as a value of `$token` variable
+
+
+```
+ruby ./buy.rb player_id max_buy_price
+```
+
+First argument is player id. Here are some sample player ids
+
+###### 180819 - lallana
+###### 164859 - wallcott
+###### 162895 - fabregas
+###### 202652 - sterling
+###### 190456 - clyne
+###### 171833 - sturridge
+
--- buy.rb
+++ buy.rb
@@ -1,46 +1,71 @@
 #!/usr/bin/env ruby
 require 'json'
-require 'pry'
-# 180819 - lallana(4000 buy)
-# 164859 - wallcott
-# 162895 - fabregas
-# 202652 - sterling
+require 'uri'
+require 'net/http'
 
-$session_id = 'dd27dd68-b147-46dc-ae47-5410a3d9434e'
-$token = '3505819857879335606'
+$session_id = '673e5ee7-5fee-4c3e-887d-13380af61c9e'
+$token = '3276982355070137448'
 
 pp 'Provide player id: ruby ./buy.rb player_id max_price' and exit unless ARGV[0]
 pp 'Provide max buy price ruby ./buy.rb player_id max_price' and exit unless ARGV[1]
 
 def fetch_auctions player_id, max_price
-  url = "https://utas.external.s3.fut.ea.com/ut/game/fifa17/transfermarket?maskedDefId=#{player_id}&start=0&num=16&type=player&maxb=#{max_price}&micr=#{[150,200,250,300,350,400,450,500,550,600,650].sample}"
-  body = `curl -s '#{url}' -H 'Cookie: __utma=103303007.1086497976.1475411054.1475411054.1475411054.1; __utmc=103303007; __utmz=103303007.1475411054.1.1.utmcsr=easports.com|utmccn=(referral)|utmcmd=referral|utmcct=/ru/fifa/ultimate-team/fut/database; _nx_mpcid=bed26e67-b135-4f4c-b9bc-7221546eedc2; utag_main=v_id:0157855a0e58001bd002eca835ff05079018a0710093c$_sn:1$_ss:0$_pn:3%3Bexp-session$_st:1475412901219$ses_id:1475411054168%3Bexp-session' -H 'Origin: https://www.easports.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4,gl;q=0.2,fr;q=0.2,de;q=0.2' -H 'Connection: keep-alive' -H 'X-UT-SID: #{$session_id}' -H 'X-Requested-With: ShockwaveFlash/23.0.0.166' -H 'X-UT-PHISHING-TOKEN: #{$token}' -H 'X-HTTP-Method-Override: GET' -H 'Pragma: no-cache' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36' -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Cache-Control: no-cache' -H 'Referer: https://www.easports.com/iframe/fut17/bundles/futweb/web/flash/FifaUltimateTeam.swf?cl=163415' -H 'X-UT-Embed-Error: true' --data-binary ' ' --compressed`
-
-  response = JSON.parse body
-
+  uri = URI "https://utas.external.s3.fut.ea.com/ut/game/fifa17/transfermarket?maskedDefId=#{player_id}&start=0&num=16&type=player&maxb=#{max_price}&micr=#{[150,200,250,300,350,400,450,500,550,600,650].sample}"
+  req = Net::HTTP::Get.new(uri)
+  prepare_request(req)
+  req['X-HTTP-Method-Override'] = 'GET'
+  res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
+    http.request(req)
+  }
+  response = JSON.parse res.body
   response['auctionInfo']
 end
 
 def bid auction_id, bid
-  url = "https://utas.external.s3.fut.ea.com/ut/game/fifa17/trade/#{auction_id}/bid"
-  body = `curl -s '#{url}' -H 'Cookie: __utmac=103303007.1086497976.1475411054.1475411054.1475411054.1; __utmc=103303007; __utmz=103303007.1475411054.1.1.utmcsr=easports.com|utmccn=(referral)|utmcmd=referral|utmcct=/ru/fifa/ultimate-team/fut/database; _nx_mpcid=bed26e67-b135-4f4c-b9bc-7221546eedc2; utag_main=v_id:0157855a0e58001bd002eca835ff05079018a0710093c$_sn:1$_ss:0$_pn:3%3Bexp-session$_st:1475412901219$ses_id:1475411054168%3Bexp-session' -H 'Origin: https://www.easports.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4,gl;q=0.2,fr;q=0.2,de;q=0.2' -H 'Connection: keep-alive' -H 'X-UT-SID: #{$session_id}' -H 'X-Requested-With: ShockwaveFlash/23.0.0.166' -H 'X-UT-PHISHING-TOKEN: #{$token}' -H 'X-HTTP-Method-Override: PUT' -H 'Pragma: no-cache' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36' -H 'Content-Type: application/json' -H 'Accept: application/json' -H 'Cache-Control: no-cache' -H 'Referer: https://www.easports.com/iframe/fut17/bundles/futweb/web/flash/FifaUltimateTeam.swf?cl=163415' -H 'X-UT-Embed-Error: true' --data-binary '{"bid":#{bid}}' --compressed`
-  response = JSON.parse body
+  uri = URI "https://utas.external.s3.fut.ea.com/ut/game/fifa17/trade/#{auction_id}/bid"
+  req = Net::HTTP::Put.new(uri)
+  prepare_request(req)
+  req['X-HTTP-Method-Override'] = 'PUT'
+  req.body = { bid: bid }.to_json
+  res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) {|http|
+    http.request(req)
+  }
+  response = JSON.parse res.body
   if response['currencies']
-    pp "Bought for #{bid}. #{response['currencies'].first['finalFunds']} gold left"
+    puts "Bought for #{bid}. #{response['currencies'].first['finalFunds']} gold left"
   else
-    pp 'Failed to buy'
+    puts 'Failed to buy'
   end
+end
+
+def prepare_request req
+  req['Cookie'] = '__utma=103303007.1086497976.1475411054.1475411054.1475411054.1; __utmc=103303007; __utmz=103303007.1475411054.1.1.utmcsr=easports.com|utmccn=(referral)|utmcmd=referral|utmcct=/ru/fifa/ultimate-team/fut/database; _nx_mpcid=bed26e67-b135-4f4c-b9bc-7221546eedc2; utag_main=v_id:0157855a0e58001bd002eca835ff05079018a0710093c$_sn:1$_ss:0$_pn:3%3Bexp-session$_st:1475412901219$ses_id:1475411054168%3Bexp-session'
+  req['Origin'] = 'https://www.easports.com'
+  req['Accept-Encoding'] = 'deflate, br'
+  req['Accept-Language'] = 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4,gl;q=0.2,fr;q=0.2,de;q=0.2'
+  req['Connection'] = 'keep-alive'
+  req['X-UT-SID'] = $session_id
+  req['X-UT-PHISHING-TOKEN'] = $token
+  req['X-Requested-With'] = 'ShockwaveFlash/23.0.0.166'
+  req['Pragma'] = 'no-cache'
+  req['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'
+  req['Content-Type'] = 'application/json'
+  req['Accept'] = 'application/json'
+  req['Cache-Control'] = 'no-cache'
+  req['Referer'] = 'https://www.easports.com/iframe/fut17/bundles/futweb/web/flash/FifaUltimateTeam.swf?cl=163415'
+  req['X-UT-Embed-Error'] = 'true'
 end
 
 loop do
   auction = fetch_auctions(ARGV[0], ARGV[1]).first
-  wait = 0.7 + rand
+  wait = 1 + rand
   if auction
-    pp "Found player for #{auction['buyNowPrice']}"
+    puts "Found player for #{auction['buyNowPrice']}"
     bid auction['tradeId'], auction['buyNowPrice']
-    pp "Waiting for #{wait} seconds"
+    #puts "Waiting for #{wait} seconds"
+    break
   else
-    pp "Nothing found. Waiting for #{wait} seconds"
+    puts "Nothing found. Waiting for #{wait} seconds"
   end
   sleep wait
 end
